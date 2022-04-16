615008b2e221caf3a20b88e1a4a53fff71bdb256
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-16 12:25:14 +0300

    ft:class - describes users class attributes

--- src/app/Classes/user.ts
+++ src/app/Classes/user.ts
@@ -1,2 +1,24 @@
 export class User {
+  pictureUrl: string;
+  name: string;
+  numberOfFollowers: number;
+  numberOfFollowing: number;
+  about: string;
+  numberOfRepos: number;
+
+  constructor(
+    pictureUrl: string,
+    name: string,
+    numberOfFollowers: number,
+    numberOfFollowing: number,
+    about: string,
+    numberOfRepos: number
+  ) {
+    this.pictureUrl = pictureUrl;
+    this.name = name;
+    this.numberOfFollowers = numberOfFollowers;
+    this.numberOfFollowing = numberOfFollowing;
+    this.about = about;
+    this.numberOfRepos = numberOfRepos;
+  }
 }
