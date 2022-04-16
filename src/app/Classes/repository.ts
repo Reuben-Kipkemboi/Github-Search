e5371f435ee790f3833cbcbb6c3aea7de1acae81
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-16 15:39:01 +0300

    ft:services - generates service to help in supply of data in different components

--- src/app/Classes/repository.ts
+++ src/app/Classes/repository.ts
@@ -1,2 +1,21 @@
 export class Repository {
+  name: string;
+  briefDescription: string;
+  languages: string;
+  timeOfUpdate: any;
+  linkToRepository: string;
+
+  constructor(
+    name: string,
+    briefDescription: string,
+    languages: string,
+    timeOfUpdate: any,
+    linkToRepository: string
+  ) {
+    this.name = name;
+    this.briefDescription = briefDescription;
+    this.languages = languages;
+    this.timeOfUpdate = timeOfUpdate;
+    this.linkToRepository = linkToRepository;
+  }
 }
---
+++ src/app/Services/dataservice.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import {User} from '../Classes/user'
+import { Repository } from '../Classes/repository';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataserviceService {
+
+ 
+}
