484c3a71570c79071de0e1345612ee8757ef6a1d
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-16 11:00:04 +0300

    ft:classes - generates user and repository classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
