7719ca9bfc20394a16193076f6ae7db26fadd4ff
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-17 07:35:55 +0300

    ft:navbar - routes all application links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { MainComponent } from './Components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "home",
+  component: HomeComponent,
+  },
+
+  {
+    path: "main",
+    component: MainComponent,
+    },
+    { path: '', redirectTo:"/home", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
